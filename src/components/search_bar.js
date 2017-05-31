4767fe40c801de7ec1c0e00cee3e678a105544ee
Rajat-Goyal <rajat1881@gmail.com>
2017-06-01 04:24:15 +0530

    search bar is <controlled> component the value user enters follws the path user -> state -> html the value passes through component state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,26 @@
 import React,{ Component } from 'react';
 
-class SearchBar extends React.Component{
+class SearchBar extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {term: ''};
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  };
 
   render() {
-    return <input/>
+    return (
+        <div>
+          <input
+              onChange={this.onInputChange}
+              value={this.state.term}
+          />
+        </div>
+    );
   }
 }
 
