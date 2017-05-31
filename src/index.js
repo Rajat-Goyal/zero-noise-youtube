20d422f6fc0c3ef4ce501df37d271c1b9a07be38
Rajat-Goyal <rajat1881@gmail.com>
2017-06-01 04:54:23 +0530

    got data from youtube api App coponent gets the data as it can pass it down to child coponents All data us usually stored as state of root componentthe da

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
-const App = () => {
-  return (
-      <div>
-        <SearchBar/>
-      </div>
-  );
-};
+const API_KEY = 'AIzaSyAZbcHvfb7_T0IZc_822pRwWH1Nk0kVg04';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'react-redux'}, (videos) => {
+      this.setState({videos: videos});
+    });
+  }
+
+  render() {
+    return (
+        <div>
+          <SearchBar/>
+        </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
