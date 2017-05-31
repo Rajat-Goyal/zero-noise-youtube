e9c9c1e508a0e16e2ffb9b9a8a58e6420614a0de
Rajat-Goyal <rajat1881@gmail.com>
2017-05-31 20:40:49 +0530

    es6 functional app component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-
-const App = function(){
-  return <div> Hello World</div>
+const App = () => {
+  return <div>Hello World!</div>
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
