174f90d5851be3eed3ae5ea2c92e4f15a86966f4
Rajat-Goyal <rajat1881@gmail.com>
2017-06-01 17:50:37 +0530

    create each vieo list item with proper details

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const VideoListItem = function(props){
+const VideoListItem = function(props) {
+  const vid = props.video;
   return (
-      <div>
-        <p>Video</p>
-      </div>
+      <li className="list-group-item">
+        <div className="video-list media">
+          <div className="media-left">
+            <img className="media-object"
+                 src={vid.snippet.thumbnails.default.url}/>
+          </div>
+
+          <div className="media-body">
+            <h5 className="media-heading"> {vid.snippet.title} </h5>
+          </div>
+
+        </div>
+      </li>
   );
 };
 
